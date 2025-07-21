6762c59c09ba203007dd7440a07c7b81347a3632
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2025-07-20 22:39:20 -0300

    fix indentation

--- src/main/java/io/github/juliana_barreto/utils/UserInput.java
+++ src/main/java/io/github/juliana_barreto/utils/UserInput.java
@@ -3,31 +3,35 @@ package io.github.juliana_barreto.utils;
 import java.util.Scanner;
 
 public class UserInput {
-    public static void ensureInt(Scanner scanner) {
-        while (!scanner.hasNextInt()) {
-            System.out.print("Entrada inválida. Digite um número inteiro: ");
-            scanner.next();
-        }
+  public static void ensureInt(Scanner scanner) {
+    while (!scanner.hasNextInt()) {
+      System.out.print("Entrada inválida. Digite um número inteiro: ");
+      scanner.next();
     }
-    public static int promptInt(Scanner scanner, int pos) {
-        System.out.print("Digite o " + pos + "º inteiro: ");
-        UserInput.ensureInt(scanner);
-        return scanner.nextInt();
+  }
+
+  public static int promptInt(Scanner scanner, int pos) {
+    System.out.print("Digite o " + pos + "º inteiro: ");
+    UserInput.ensureInt(scanner);
+    return scanner.nextInt();
+  }
+
+  public static int promptInt(Scanner scanner, String msg) {
+    System.out.print(msg);
+    UserInput.ensureInt(scanner);
+    return scanner.nextInt();
+  }
+
+  public static void ensureDouble(Scanner scanner) {
+    while (!scanner.hasNextDouble()) {
+      System.out.print("Entrada inválida. Digite um número real separado por \",\": ");
+      scanner.next();
     }
-    public static int promptInt(Scanner scanner, String msg) {
-        System.out.print(msg);
-        UserInput.ensureInt(scanner);
-        return scanner.nextInt();
-    }
-    public static void ensureDouble(Scanner scanner) {
-        while (!scanner.hasNextDouble()) {
-            System.out.print("Entrada inválida. Digite um número real separado por \",\": ");
-            scanner.next();
-        }
-    }
-    public static double promptDouble(Scanner scanner, String msg) {
-        System.out.print(msg);
-        UserInput.ensureDouble(scanner);
-        return scanner.nextDouble();
-    }
+  }
+
+  public static double promptDouble(Scanner scanner, String msg) {
+    System.out.print(msg);
+    UserInput.ensureDouble(scanner);
+    return scanner.nextDouble();
+  }
 }
