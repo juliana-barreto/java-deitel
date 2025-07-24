22aa45ad12d50a3e8e4b404353822696a1f0bdbf
Juliana <89608026+juliana-barreto@users.noreply.github.com>
2025-07-23 22:53:53 -0300

    Refactor UserInput class for improved input handling and localization

--- src/main/java/io/github/juliana_barreto/utils/UserInput.java
+++ src/main/java/io/github/juliana_barreto/utils/UserInput.java
@@ -3,15 +3,19 @@ package io.github.juliana_barreto.utils;
 import java.util.Scanner;
 
 public class UserInput {
+
+  private UserInput() {
+  }
+
   public static void ensureInt(Scanner scanner) {
     while (!scanner.hasNextInt()) {
-      System.out.print("Entrada inválida. Digite um número inteiro: ");
-      scanner.next();
+      System.out.print("Invalid input. Please enter an integer: ");
+      scanner.next(); // Consume the invalid token
     }
   }
 
   public static int promptInt(Scanner scanner, int pos) {
-    System.out.print("Digite o " + pos + "º inteiro: ");
+    System.out.print("Enter integer #" + pos + ": ");
     UserInput.ensureInt(scanner);
     return scanner.nextInt();
   }
@@ -23,15 +27,17 @@ public class UserInput {
   }
 
   public static void ensureDouble(Scanner scanner) {
+    scanner.useLocale(Locale.US);
     while (!scanner.hasNextDouble()) {
-      System.out.print("Entrada inválida. Digite um número real separado por \",\": ");
-      scanner.next();
+      System.out.print("Invalid input. Please enter a decimal number: ");
+      scanner.next(); // Consume the invalid token
     }
   }
+}
 
-  public static double promptDouble(Scanner scanner, String msg) {
-    System.out.print(msg);
-    UserInput.ensureDouble(scanner);
-    return scanner.nextDouble();
-  }
+public static double promptDouble(Scanner scanner, String msg) {
+  System.out.print(msg);
+  UserInput.ensureDouble(scanner);
+  return scanner.nextDouble();
+}
 }
